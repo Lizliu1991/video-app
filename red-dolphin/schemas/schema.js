47d758db7167a4e479e1cc4cb2ca46008fae8556
lizzy.liu <lavialiu@hotmail.com>
2022-07-14 14:08:12 +1000

    initial commit

---
+++ red-dolphin/schemas/schema.js
@@ -0,0 +1,18 @@
+// First, we must import the schema creator
+import createSchema from 'part:@sanity/base/schema-creator'
+
+// Then import schema types from any plugins that might expose them
+import schemaTypes from 'all:part:@sanity/base/schema-type'
+import post from './post'
+import comments from './comments'
+import postedBy from './postedBy'
+import user from './user'
+
+// Then we give our schema to the builder and provide the result to Sanity
+export default createSchema({
+  name: 'default',
+  types: schemaTypes.concat([
+    //types
+    post, user, comments,postedBy
+  ]),
+})
